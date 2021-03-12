58b6dc094c2d15d539c62cd7f7f9233d7931b36a
adleris <alexander.leris@gmail.com>
2021-03-13 00:23:47 +1100

    add subscriber to gps

--- scripts/vicpark_listener.py
+++ scripts/vicpark_listener.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 
 import ast
 
-def callback(data):
+def lm_callback(data):
     # ugly but it works: parse the string representation of the list of tuples
     # back into the appropriate data structure
     #
@@ -16,11 +16,20 @@ def callback(data):
     # (this took a long time to figure out)
     datalist =  ast.literal_eval(data[1:-1])
 
+    print "<---- LANDMARK READ ---->" 
     print datalist
+
+def gps_callback(data):
+    data = str(data)[5:-1].replace('"','').strip().replace("[ ","[").replace(" ",",")
+    data = ast.literal_eval(data)
+    print  "<==== GPS READ ====>"
+    print  data
 
 def VicParkListener():
     rospy.init_node("VicParkListener", anonymous=True)
-    rospy.Subscriber("landmarks", String, callback)
+    rospy.Subscriber("landmarks", String, lm_callback)
+
+    rospy.Subscriber("gps", String, gps_callback)
     rospy.spin()
 if __name__ == "__main__":
     VicParkListener()
