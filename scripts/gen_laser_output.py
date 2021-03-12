7ee34dd7bddb907dc2aa05da8ea3a78c55019e7f
adleris <alexander.leris@gmail.com>
2021-03-12 17:43:41 +1100

    configure publisher to use vicpark matlab data

--- scripts/gen_laser_output.py
+++ scripts/gen_laser_output.py
@@ -5,16 +5,28 @@ from std_msgs.msg import String
 import MatlabImporter
 
 def VicParkPublisher():
-    MI = MatlabImporter()
-
-
-    pub = rospy.Publisher('vic_park_data', String, queue_size=10)
+    MI = MatlabImporter.MatlabImporter()
+    pub_laser = rospy.Publisher('landmarks', String, queue_size=10)
+    pub_gps   = rospy.Publisher('gps', String, queue_size=10)
+    pub_dr    = rospy.Publisher('dr', String, queue_size=10)
+    
     rospy.init_node('VicPark', anonymous=True)
     rate = rospy.Rate(10)
+
     while not rospy.is_shutdown():
-        data = VicParkImporter.get_next_laser_string()
+        # publish to all topics
+        data = MI.next_lm()
         rospy.loginfo(data)
-        pub.publish(data)
+        pub_laser.publish(data)
+
+        data = MI.next_gps()
+        rospy.loginfo(data)
+        pub_gps.publish(data)
+
+        data = MI.next_dr()
+        rospy.loginfo(data)
+        pub_dr.publish(data)
+
         rate.sleep()
 
 
